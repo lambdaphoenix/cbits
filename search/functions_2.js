014de2330ed1ee7908d088e7b15fe74390280cc2
lambdaphoenix <lambdaphoenix@users.noreply.github.com>
2025-07-16 17:12:00 +0000

    deploy: 95c01b8db1e03904a89b7147317ce65d5c7de686

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['init_5fcpu_5fdispatch_0',['init_cpu_dispatch',['../compat_8h.html#a9256aa51e475beabf9cd975766084809',1,'compat.h']]]
+  ['init_5fcpu_5fdispatch_0',['init_cpu_dispatch',['../compat_8h.html#a9256aa51e475beabf9cd975766084809',1,'init_cpu_dispatch(void):&#160;compat_dispatch.c'],['../compat__dispatch_8c.html#a9256aa51e475beabf9cd975766084809',1,'init_cpu_dispatch(void):&#160;compat_dispatch.c']]]
 ];
