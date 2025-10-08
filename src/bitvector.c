64d91aa52b3b0906953a3796d4c20662f9b495cb
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2025-10-08 16:38:03 +0200

    ref: improve overhead in rank, contains

--- src/bitvector.c
+++ src/bitvector.c
@@ -107,13 +107,31 @@ bv_build_rank(BitVector *bv)
             super_total += cbits_popcount_block(&bv->data[base]);
         }
         else {
-            for (size_t w = base; w < end; ++w) {
+            size_t w = base;
+            for (; w + 3 < end; w += 4) {
+                super_total += cbits_popcount64(bv->data[w]);
+                super_total += cbits_popcount64(bv->data[w + 1]);
+                super_total += cbits_popcount64(bv->data[w + 2]);
+                super_total += cbits_popcount64(bv->data[w + 3]);
+            }
+            for (; w < end; ++w) {
                 super_total += cbits_popcount64(bv->data[w]);
             }
         }
 
         size_t acc = 0;
-        for (size_t w = base; w < end; ++w) {
+        size_t w = base;
+        for (; w + 3 < end; w += 4) {
+            bv->block_rank[w] = (uint16_t) acc;
+            acc += cbits_popcount64(bv->data[w]);
+            bv->block_rank[w + 1] = (uint16_t) acc;
+            acc += cbits_popcount64(bv->data[w + 1]);
+            bv->block_rank[w + 2] = (uint16_t) acc;
+            acc += cbits_popcount64(bv->data[w + 2]);
+            bv->block_rank[w + 3] = (uint16_t) acc;
+            acc += cbits_popcount64(bv->data[w + 3]);
+        }
+        for (; w < end; ++w) {
             bv->block_rank[w] = (uint16_t) acc;
             acc += cbits_popcount64(bv->data[w]);
         }
@@ -162,23 +180,50 @@ bv_contains_subvector(const BitVector *A, const BitVector *B)
     }
 
     const size_t max_pos = A->n_bits - B->n_bits;
-    const size_t B_words = B->n_words;
     const unsigned B_tail = (unsigned) (B->n_bits & 63);
     const uint64_t tail_mask = B_tail ? ((1ULL << B_tail) - 1) : UINT64_MAX;
 
+    if (B->n_bits <= 64) {
+        uint64_t needle = B->data[0] & tail_mask;
+        for (size_t pos = 0; pos <= max_pos; ++pos) {
+            size_t w_off = pos >> 6;
+            unsigned b_off = pos & 63;
+
+            uint64_t lo = A->data[w_off];
+            uint64_t hi = (w_off + 1 < A->n_words) ? A->data[w_off + 1] : 0ULL;
+            uint64_t window = (lo >> b_off) | (hi << (64 - b_off));
+
+            if ((window & tail_mask) == needle) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const size_t B_words = B->n_words;
     for (size_t pos = 0; pos <= max_pos; ++pos) {
         const size_t w_off = pos >> 6;
         const unsigned b_off = pos & 63;
 
-        bool match = true;
-        for (size_t j = 0; j < B_words; ++j) {
-            const uint64_t wordB = B->data[j];
-            uint64_t wordA = A->data[w_off + j] >> b_off;
-            if (b_off && w_off + j + 1 < A->n_words) {
-                wordA |= A->data[w_off + j + 1] << (64 - b_off);
+        {
+            uint64_t lo = A->data[w_off];
+            uint64_t hi = (w_off + 1 < A->n_words) ? A->data[w_off + 1] : 0ULL;
+            uint64_t aw0 = (lo >> b_off) | (hi << (64 - b_off));
+            uint64_t mask0 = (B_words == 1) ? tail_mask : UINT64_MAX;
+            if ((aw0 & mask0) != (B->data[0] & mask0)) {
+                continue;
             }
-            uint64_t mask = (j + 1 == B_words) ? tail_mask : UINT64_MAX;
-            if ((wordA & mask) != (wordB & mask)) {
+        }
+
+        bool match = true;
+        for (size_t j = 1; j < B_words; ++j) {
+            uint64_t lo = A->data[w_off + j];
+            uint64_t hi =
+                (w_off + j + 1 < A->n_words) ? A->data[w_off + j + 1] : 0ULL;
+            uint64_t wordA = (lo >> b_off) | (hi << (64 - b_off));
+            uint64_t mask = (B_words == j + 1) ? tail_mask : UINT64_MAX;
+
+            if ((wordA & mask) != (B->data[j] & mask)) {
                 match = false;
                 break;
             }
